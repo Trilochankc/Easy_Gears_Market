cbd49bcec32cf0ab1cadda5a5cd3ba81301529c3
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2019-12-02 13:17:44 +0545

    login admin

--- app/src/main/java/com/trilochan/easy_gears_market/MainActivity.java
+++ app/src/main/java/com/trilochan/easy_gears_market/MainActivity.java
@@ -3,12 +3,23 @@ package com.trilochan.easy_gears_market;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etUsername, etpswd;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.etusername);
+        etpswd = findViewById(R.id.etpswd);
+        btnLogin = findViewById(R.id.bottom);
+
+
     }
 }
